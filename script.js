081eefb85102cb8a3a8319ac6b5155cbccb8e0c2
guptaalakshay <guptaa.lakshay@gmail.com>
2026-06-25 23:52:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+console.log("EchoVerse Loaded");
 
+document.querySelectorAll(".feature-card")
+.forEach(card => {
+
+card.addEventListener("mouseenter", () => {
+card.style.transform = "translateY(-8px)";
+});
+
+card.addEventListener("mouseleave", () => {
+card.style.transform = "translateY(0px)";
+});
+
+});
